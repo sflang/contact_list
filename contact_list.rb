f0048374f3ce7101ac1afc08ece78ac903e1bccc
Steve Lang <lang123@yahoo.com>
2015-11-04 00:09:24 +0000

    moved exceptions to separate file

--- contact_list.rb
+++ contact_list.rb
@@ -3,13 +3,13 @@
 require_relative 'contact'
 require_relative 'contact_database'
 require_relative 'application'
-#require_relative 'exceptions'
+require_relative 'exceptions'
 
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
 
-class BadArgument < StandardError
-end
+# class BadArgument < StandardError
+# end
 
 def help
   puts "Here is a list of available commands:"
